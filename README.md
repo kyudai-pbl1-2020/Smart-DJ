e31c68ea3bd796833ea4f3d3ab59e506ddf7ec38
Yamate Kyosuke <yamate@poslnoMacBook-Pro.local>
2020-05-22 16:34:06 +0900

    Add Dockerfile

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
-# README
+# MQTTアクセス用テンプレート / MQTT Usage Templae
 
-- test
+## 概要 / Overview
+このアーカイブに格納されているファイル群はPythonを使って[Beebotte](http://beebotte.com)にMQTTでアクセスするサンプルを提供するものである。
+環境を統一するため、Dockerコンテナとして提供する。
+
+Files in this archive show a simple example accessing [Beebotte](http://beebotte.com) using MQTT protocol.
+To avoid problems caused by different operating systems, the example is embedded on a docker container.
+
+## ファイル構成 / Files and Directories
+
+ファイル/ディレクトリ | 説明
+----- | -----
+``src/`` | pythonソースが格納されているディレクトリ。Dockerコンテナ上ではvolumeとしてマウントされる。
+``src/client_advanced.py`` | [mqbeebotte](https://github.com/pman0214/mqbeebotte/) を使ったBeebotteアクセスのサンプル。
+``src/config.py`` | ``client_advanced.py``用の設定ファイル。**このファイルは絶対にcommitしないこと。**
+``docker-compose.yml`` | ``docker-compose``コマンド用の設定YAMLファイル。
+``Dockerfile`` | docker環境構築用のDockerfile。
+``requirements.txt`` | python環境構築用のライブラリリスト。
+``README.md`` | このファイル。
+
+File/Directory | Details
+----- | -----
+ ``src/`` | is a directory containing python source files.  The docker container mounts the ``src/`` directory as a volume.
+``src/client_advanced.py`` | Beebotte access sample using [mqbeebotte](https://github.com/pman0214/mqbeebotte/).
+``src/config.py`` | is a config file for ``client_advanced.py``.  **DO NOT COMMIT THIS FILE**.
+``docker-compose.yml`` | is a config file for ``docker-compose`` command.
+``Dockerfile`` | creates the docker contianer.
+``requirements.txt`` | is a list of python libraries.
+``README.md`` | is this file.
+
+## 使い方 / Usage
+
+1. docker、docker-composeはあらかじめインストールしておく。
+2. 必要であれば``docker-compose.yml``を修正し、初期起動するpythonコードを書いておく。初期状態では何も起動しない。特定のスクリプトを実行する場合はコメントアウトされている``command``行のように記述する。
+3. ``docker-compose up -d``する。
+4. ちょっと試しに実行したいのであれば``docker-compose exec beebotte /bin/bash``してシェルを起動し、その上でpythonを実行する。なお、``src/``は``/app/src``にマウントされている。
+5. 終了したいときは``docker-compose stop``（停止するとき）または``docker-compose down``（停止してdockerコンテナも削除するとき）する。
+
+--
+
+1. Install docker and docker-compose on your machine (or on a target machine).
+2. If you want to kick any python script, modify ``docker-compose.yml``.  ``docker-compose.yml`` in this archive does not start any script.  If you want to start a specific script when the container starts, write a start up command on a ``command`` line.
+3. Execute ``docker-compose up -d``.
+4. If you want to try any python script, execute ``docker-compose exec beebotte /bin/bash`` to start shell on the docker container and then execute any command including python.  Note that ``src/`` directory is mounted on ``/app/src`` as a volume.
+5. When you want to stop docker, execute ``docker-compose stop`` (just stopping) or ``docker-compose down`` (stop and remove the container).
---
+++ src/client_advanced.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+This sample is designed for client module debugging.
+
+Prepare for Beebotte access information in the config file 'config.py'
+including:
+    channel_token : str
+        Channel token.
+    ca_cert : str
+        Path to a SSL server certificate.
+    topic_base : str
+        Topic name, i.e., 'channel/resource'.
+        None to use non-SSL connection.
+    unit : int
+        Unit number.
+"""
+
+#
+# Copyright (c) 2020, Shigemi ISHIDA
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import time
+import mqbeebotte
+
+#=========================================================================
+# This sample comes with config.py.
+# Please modify config.py before executing this sample.
+#=========================================================================
+def on_connect(client, userdata, flags, respons_code):
+    print('Connected to Beebotte')
+    return
+
+#--------------------------------------------------------------------------
+def on_message(client, userdata, msg):
+    print('[{}] {}'.format(msg.topic, str(msg.payload)))
+    return
+
+#=========================================================================
+# Before execute any MQTT client code, you need to create a channel and resource
+# on Beebotte.
+
+# Load config file.
+import config
+
+# Create an client instance.
+try:
+    ca_cert = config.ca_cert
+except AttributeError:
+    ca_cert = None
+client = mqbeebotte.client(ca_cert=ca_cert)
+
+# And connect to Beebotte.
+client.connect(config.channel_token, on_connect=on_connect, on_message=on_message)
+# Start network loop thread.
+# Thread is running in a non-blocking manner.
+client.start()
+
+#--------------------------------------------------------------------------
+# You can do anything after this including executing subscribe and publish.
+#--------------------------------------------------------------------------
+
+# Subscribe to mutiple topics.
+# Generate target topic names.
+topics = [
+    '#',
+]
+sub_topics = list(map(lambda x: config.topic_base + '/' + x, topics))
+print('Subscribe to {}'.format(', '.join(sub_topics)))
+# Give a list to subscribe() to subscribe to multiple topics.
+client.subscribe(sub_topics)
+print('Subscribe to {}/unit{:d}/2'.format(config.topic_base, config.unit))
+client.subscribe('pbl1/unit{:d}/2'.format(config.unit))
+
+for cnt in range(1,4):
+    # I usually hook Ctrl-C to close connection properly.
+    try:
+        time.sleep(5)
+        # Publish a message to a topic 'pbl1/unitX/x'
+        print('Publish a message to pbl1/unit{:d}/{:d}'.format(config.unit, cnt))
+        client.publish('pbl1/unit{:d}/{:d}'.format(config.unit, cnt), 'test count {:d}'.format(cnt))
+    except KeyboardInterrupt:
+        print('Stop requested.')
+        break
+
+# wait for the final on_message call.
+time.sleep(5)
+
+# Stop network loop thread.
+# When block_wait is set to True, stop() blocks until the thread stops.
+# Default block_wait is False.  In a non-blocking manner, you need to
+# wait until the thead stops using client.join().
+client.stop(block_wait=True)
+
+# Disconnect and removes the client instance.
+del client
