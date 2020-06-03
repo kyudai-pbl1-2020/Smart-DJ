4c0a251df9ba9e45e481637d280a19467c174f0b
nakagawa-t <ae18nakagawa@g.kumamoto-nct.ac.jp>
2020-06-03 18:10:36 +0900

    correct modelu path

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import numpy as np
 import pandas as pd
-from . import csv2dataset
-from . import model_build
+#from . import csv2dataset 
+#from . import model_build
+import csv2dataset 
+import model_build
 
 #データの読み込み
-df = csv2dataset.dataframe_exporter("weather_data/*") 
+df = csv2dataset.dataframe_exporter("./weather_data/*") 
 data = df.drop("weather",axis=1)
 target = df["weather"]
 
 model_build.build(data,target)
-
--- src/model_build.py
+++ src/model_build.py
@@ -2,11 +2,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import lightgbm as lgb
-from sklearn.metrics import log_loss
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import log_loss,accuracy_score
 from sklearn.model_selection import train_test_split
 import optuna
-from . import csv2dataset
+import pickle
+import csv2dataset
+
 
  
 #モデルの構築
@@ -14,7 +15,7 @@ def build(data,target):
     
     #ハイパラメータのチューニング
     study = optuna.create_study(direction='maximize')
-    study.optimize(objective, n_trials=100)
+    study.optimize(objective_data(data,target), n_trials=100)
  
     print('Number of finished trials:', len(study.trials))
     print('Best trial:', study.best_trial.params)
@@ -34,40 +35,54 @@ def build(data,target):
 
         gbm = lgb.train(params, dtrain)
         preds = gbm.predict(val_x)
-        pred_labels = np.rint(preds)
-        score = accuracy_score(va_y,va_pred)
+        # 最尤と判断したクラスの値にする
+        preds_max = np.argmax(preds, axis=1)  
+        
+        # 精度 (Accuracy) を計算する
+        score = sum(val_y == preds_max) / len(val_y)
         scores.append(score)
+    print(f'logloss: {np.mean(scores):.4f}')
 
     #データ全体を使って学習
     all_train = lgb.Dataset(data, label=target)
     gbm = lgb.train(params, all_train)
+
+    #モデルを保存
+    file = 'trained_model.pkl'
+    pickle.dump(clf, open(file, 'wb'))
+
+
     """
 
 
 #パラメータのチューニング
-def objective(trial):
+def objective_data(data,target):
+    def objective(trial):
 
-    #データの読み込み
-    df = csv2dataset.dataframe_exporter("weather_data/*") 
-    data = df.drop("weather",axis=1)
-    target = df["weather"]
-    train_x, test_x, train_y, test_y = train_test_split(data, target, test_size=0.25)
-    dtrain = lgb.Dataset(train_x, label=train_y)
- 
-    param = {
-        'objective': 'multiclass',
-        'metric': 'multi_logloss',
-        'lambda_l1': trial.suggest_loguniform('lambda_l1', 1e-8, 10.0),
-        'lambda_l2': trial.suggest_loguniform('lambda_l2', 1e-8, 10.0),
-        'num_leaves': trial.suggest_int('num_leaves', 2, 256),
-        'feature_fraction': trial.suggest_uniform('feature_fraction', 0.4, 1.0),
-        'bagging_fraction': trial.suggest_uniform('bagging_fraction', 0.4, 1.0),
-        'bagging_freq': trial.suggest_int('bagging_freq', 1, 7),
-        'min_child_samples': trial.suggest_int('min_child_samples', 5, 100),
-    }
- 
-    gbm = lgb.train(param, dtrain)
-    preds = gbm.predict(test_x)
-    pred_labels = np.rint(preds)
-    accuracy = accuracy_score(test_y, pred_labels)
-    return accuracy
+        #データの読み込み
+        #df = csv2dataset.dataframe_exporter("./weather_data/*") 
+        #data = df.drop("weather",axis=1)
+        #target = df["weather"]
+        train_x, test_x, train_y, test_y = train_test_split(data, target, test_size=0.25)
+        dtrain = lgb.Dataset(train_x, label=train_y)
+    
+        param = {
+            'objective': 'multiclass',
+            'metric': 'multi_logloss',
+            'lambda_l1': trial.suggest_loguniform('lambda_l1', 1e-8, 10.0),
+            'lambda_l2': trial.suggest_loguniform('lambda_l2', 1e-8, 10.0),
+            'num_leaves': trial.suggest_int('num_leaves', 2, 256),
+            'feature_fraction': trial.suggest_uniform('feature_fraction', 0.4, 1.0),
+            'bagging_fraction': trial.suggest_uniform('bagging_fraction', 0.4, 1.0),
+            'bagging_freq': trial.suggest_int('bagging_freq', 1, 7),
+            'min_child_samples': trial.suggest_int('min_child_samples', 5, 100),
+        }
+    
+        gbm = lgb.train(param, dtrain)
+        preds = gbm.predict(test_x)
+        preds_max = np.argmax(preds, axis=1)  
+        # 最尤と判断したクラスの値にする
+        # 精度 (Accuracy) を計算する
+        accuracy = sum(test_y == preds_max) / len(test_y)
+        return accuracy
+    return objective
