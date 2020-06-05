916bac1b7d4aec1424a34cf92779f968fe29cb45
nakagawa-t <ae18nakagawa@g.kumamoto-nct.ac.jp>
2020-06-05 13:03:56 +0900

    modify

--- src/model_build.py
+++ src/model_build.py
@@ -67,13 +67,15 @@ def objective_data(data,target):
             'objective': 'multiclass',
             'metric': 'multi_logloss',
             'num_class': 16,
-            'lambda_l1': trial.suggest_loguniform('lambda_l1', 1e-8, 10.0),
-            'lambda_l2': trial.suggest_loguniform('lambda_l2', 1e-8, 10.0),
-            'num_leaves': trial.suggest_int('num_leaves', 2, 256),
-            'feature_fraction': trial.suggest_uniform('feature_fraction', 0.4, 1.0),
-            'bagging_fraction': trial.suggest_uniform('bagging_fraction', 0.4, 1.0),
-            'bagging_freq': trial.suggest_int('bagging_freq', 1, 7),
-            'min_child_samples': trial.suggest_int('min_child_samples', 5, 100),
+            'boosting': 'gbdt',
+            #'lambda_l1': trial.suggest_loguniform('lambda_l1', 1e-8, 10.0),
+            #'lambda_l2': trial.suggest_loguniform('lambda_l2', 1e-8, 10.0),
+            #'num_leaves': trial.suggest_int('num_leaves', 2, 256),
+            #'feature_fraction': trial.suggest_uniform('feature_fraction', 0.4, 1.0),
+            #'bagging_fraction': trial.suggest_uniform('bagging_fraction', 0.4, 1.0),
+            #'bagging_freq': trial.suggest_int('bagging_freq', 1, 7),
+            'max_depth' : trial.suggest_int('max_depth', 3, 9),
+            'min_data_in_leaf':2,
         }
     
         gbm = lgb.train(param, dtrain)
