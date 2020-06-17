cd5c849b7c8d3d87db98949a08588fb6327727a5
tanakataisei <tanaka.taisei.129@s.kyushu-u.ac.jp>
2020-06-17 17:09:20 +0900

    predict emotion from face image

--- src/emotion_predict.py
+++ src/emotion_predict.py
@@ -11,9 +11,6 @@ def emotion_recognition(image_path): #predict emotion from image of
     classes = ({0:'angry',1:'disgust',2:'fear',3:'happy',
             4:'sad',5:'surprise',6:'neutral'})
 
-    #image_path = './images/happy.jpg'
-    #image_path = '.images/sad.jpg'
-
     img = image.load_img(image_path, grayscale=True , target_size=(64, 64))
     img_array = image.img_to_array(img)
     pImg = np.expand_dims(img_array, axis=0) / 255
@@ -23,11 +20,6 @@ def emotion_recognition(image_path): #predict emotion from image of
     emotions_XCEPTION = load_model(model_path, compile=False)
 
     prediction = emotions_XCEPTION.predict(pImg)[0]
-
-    #convert the model into tf.js model
-    save_path = '../nodejs/static/emotion_XCEPTION'
-    tfjs.converters.save_keras_model(emotions_XCEPTION, save_path)
-    print("[INFO] saved tf.js emotion model to disk..")
 
     top_indices = prediction.argsort()[-5:][::-1]
     result = [(classes[i] , prediction[i]) for i in top_indices]
