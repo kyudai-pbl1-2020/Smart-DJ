3a87369af77a0e3582d4dc5d1f21c88e2426cce0
Yamate Kyosuke <yamate@poslnoMacBook-Pro-2.local>
2020-07-03 16:37:07 +0900

    Add sensor page

--- src/webtest.py
+++ src/webtest.py
@@ -17,5 +17,9 @@ def reply():
     # return answer
     return jsonify(result)
 
+@app.route("/sensor")
+def sensor():
+    return "route get. Hello!"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000,debug=True)
