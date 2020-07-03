e27d5d95eff8d231f9728c12ee3f47c0f5fa4d75
Yamate Kyosuke <yamate@poslnoMacBook-Pro-2.local>
2020-07-03 17:26:34 +0900

    Add sensor page

--- src/webtest.py
+++ src/webtest.py
@@ -1,5 +1,12 @@
 from flask import Flask, jsonify, request
 import json
+import numpy as np
+import pandas as pd
+from weather import csv2dataset
+from weather import model_build
+from weather import pred
+from sensor import sensor_data_processor as sdp
+
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
@@ -19,7 +26,24 @@ def reply():
 
 @app.route("/sensor")
 def sensor():
-    return "route get. Hello!"
+    df = csv2dataset.dataframe_exporter("./weather/weather_data/*") 
+    df = csv2dataset.convert_weather(df,"./weather/convert_weather_token.csv")
+    data = df.drop("weather",axis=1)
+    target = df["weather"]
+
+    #モデルの構築
+    #model_build.build(data,target)
+
+    #予測用データ読み込み (list['month','hour','temperature','pressure','humidity'])
+    pred_data = sdp.subscribe_sensor_data()
+    #pred_data = [6,17,27.12,998.2,64.22] #テスト用
+    #print(pred_data)
+    #予測データの変換
+
+    if len(pred_data) != 0:
+        pred_data = pd.Series(pred_data, index=['month','hour','temperature','pressure','humidity'])
+        key = pred.pred(pred_data)
+        return(str(key) + 'sensor')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000,debug=True)
