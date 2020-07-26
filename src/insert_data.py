5e23072d86048f28540cc53b468ecc9b2631b28e
Yamate Kyosuke <yamate@poslnoMacBook-Pro-2.local>
2020-07-26 19:30:36 +0900

    insert sensor data into database

--- src/insert_data.py
+++ src/insert_data.py
@@ -3,26 +3,31 @@ import mysql.connector
 
 # Dockerを使う場合で、初期設定の場合hostは"192.168.99.100"
 # MySQLのユーザやパスワード、データベースはdocker-compose.ymlで設定したもの
-connector = mysql.connector.connect(
-            user='user',
-            password='password',
-            host='mysql_db',
-            database='sample_db',
-            port='3306')
+for i in range(0, 4):
+    connector = mysql.connector.connect(
+                user='user',
+                password='password',
+                host='mysql_db',
+                database='sample_db',
+                port='3306')
 
-cursor = connector.cursor()
-#pred_data = sdp.subscribe_sensor_data()
-#pred_data = [6,17,27.12,998.2,64.22] #テスト用
-pred_data = [7,26,30.12,987.6,78.90] #テスト用2
-inserted_data = "INSERT INTO users (month,day,temperature,pressure,humidity) VALUES ({}, {}, {}, {}, {});".format(pred_data[0], pred_data[1], pred_data[2], pred_data[3], pred_data[4])
-cursor.execute(inserted_data)
-cursor.execute("select * from users")
-disp = ""
-for row in cursor.fetchall():
-    disp = "ID:" + str(row[0]) + "<br>month:" + str(row[1]) + "<br>day:" + str(row[2]) + "<br>temperature:" + str(row[3]) + "<br>pressure:" + str(row[4]) + "<br>humidity:" + str(row[5])
+    cursor = connector.cursor()
+    pred_data = sdp.subscribe_sensor_data()
+    if (pred_data != []):
+        inserted_data = "INSERT INTO users (month,day,temperature,pressure,humidity) VALUES ({}, {}, {}, {}, {});".format(pred_data[0], pred_data[1], pred_data[2], pred_data[3], pred_data[4])
+        cursor.execute(inserted_data)
+    #pred_data = [6,17,27.12,998.2,64.22] #テスト用
+    #pred_data = [7,26,30.12,987.6,78.90] #テスト用2
+    
 
-cursor.close
-connector.commit()
-connector.close
+    '''
+    cursor.execute("select * from users")
+    disp = ""
+    for row in cursor.fetchall():
+        disp = "ID:" + str(row[0]) + "<br>month:" + str(row[1]) + "<br>day:" + str(row[2]) + "<br>temperature:" + str(row[3]) + "<br>pressure:" + str(row[4]) + "<br>humidity:" + str(row[5])
+    print(disp)
+    '''
 
-print(disp)
+    cursor.close
+    connector.commit()
+    connector.close
